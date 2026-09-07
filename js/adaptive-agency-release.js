fd7a4a5441dc3629f591edb82273628cbed124d6
siyayoacademy <psycozoica@gmail.com>
2026-09-07 12:03:41 -0300

    Compose learner agency with conservative WAIT release

---
+++ js/adaptive-agency-release.js
@@ -0,0 +1,37 @@
+(function (root, factory) {
+  const api = factory(
+    typeof module === 'object' && module.exports ? require('./adaptive-learner-agency.js') : root.AdaptiveLearnerAgency,
+    typeof module === 'object' && module.exports ? require('./adaptive-wait-release.js') : root.AdaptiveWaitRelease
+  );
+  if (typeof module === 'object' && module.exports) module.exports = api;
+  else root.AdaptiveAgencyRelease = api;
+})(typeof globalThis !== 'undefined' ? globalThis : this, function (AdaptiveLearnerAgency, AdaptiveWaitRelease) {
+  function evaluateAgencyRelease(waitClassification = null, learnerEvent = null) {
+    if (!AdaptiveLearnerAgency || typeof AdaptiveLearnerAgency.evaluateAgency !== 'function') return null;
+    if (!AdaptiveWaitRelease || typeof AdaptiveWaitRelease.evaluateRelease !== 'function') return null;
+
+    const agencyEvaluation = AdaptiveLearnerAgency.evaluateAgency(waitClassification, learnerEvent);
+
+    if (!agencyEvaluation || agencyEvaluation.status !== 'RESUME_AUTHORIZATION_ELIGIBLE') {
+      return {
+        agencyEvaluation,
+        releaseEvaluation: waitClassification && waitClassification.state
+          ? AdaptiveWaitRelease.evaluateRelease(waitClassification, null)
+          : null
+      };
+    }
+
+    const groundedReleaseEvent = {
+      observed: true,
+      authorizesResume: true,
+      type: agencyEvaluation.eventType || learnerEvent?.type || null
+    };
+
+    return {
+      agencyEvaluation,
+      releaseEvaluation: AdaptiveWaitRelease.evaluateRelease(waitClassification, groundedReleaseEvent)
+    };
+  }
+
+  return { evaluateAgencyRelease };
+});
